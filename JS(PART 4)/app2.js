d7d6abf212f9dded2ed791ebf7a48bcfb2974c71
KISHLAY568 <kishlayk568@gmail.com>
2024-02-04 19:25:21 +0530

    first commit

---
+++ JS(PART 4)/app2.js
@@ -0,0 +1,7 @@
+let car = {
+    name: "Buggati",
+    model: "centadici",
+    color: "black"
+};
+
+console.log(car);
---
+++ JS(PART 4)/app3.js
@@ -0,0 +1,9 @@
+const Person = {
+    name: "Kishlay Kumar",
+    age: "20", city: "Mokama"
+};
+console.log(Person);
+Person.city = "New York";
+console.log(Person);
+Person.Country = "United States";
+console.log(Person);
