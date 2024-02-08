0e780cb2fdfebfade4e214e985ac5629ba17b5e9
KISHLAY568 <kishlayk568@gmail.com>
2024-02-08 18:52:49 +0530

    first commit

--- JS(PART 8)/destructuring.js
+++ JS(PART 8)/destructuring.js
@@ -1,6 +1,20 @@
-let names = ["tony","bruce","peter","natasha","chris","McMahon"];
-let [winner,runnerup,...others] = names;
+let names = ["tony", "bruce", "peter", "natasha", "chris", "McMahon"];
+let [winner, runnerup, ...others] = names;
 
 console.log(winner);
 console.log(runnerup);
 console.log(others);
+
+let info = {
+    username: "xyz@gmail.com",
+    password: "abcd",
+    city: "Mumbai"
+}
+
+let { username, password } = info;
+console.log(username);
+console.log(password);
+let { username: alpha, password: gamma, city: place = "Pune" } = info;
+console.log(alpha);
+console.log(gamma);
+console.log(place);
