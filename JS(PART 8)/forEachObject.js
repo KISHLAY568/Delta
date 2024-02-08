fa313c44d3497e05378a5fc7710747a4a5f8e2e0
KISHLAY568 <kishlayk568@gmail.com>
2024-02-08 16:24:50 +0530

    first commit

---
+++ JS(PART 8)/forEachObject.js
@@ -0,0 +1,20 @@
+let arr = [{
+    name: "Dhruv",
+    marks: 88
+},
+{
+    name: "Kishlay",
+    marks: 82
+
+},
+{
+    name: "Aman",
+    marks: 87
+}];
+
+arr.forEach((student) => {
+    console.log(student);
+})
+arr.forEach((student) => {
+    console.log(student.marks);
+})
---
+++ JS(PART 8)/map.js
@@ -0,0 +1,6 @@
+let num = [1, 2, 3, 4, 5];
+let double = num.map((n) => {
+    return n * n;
+})
+console.log(num);
+console.log(double);
