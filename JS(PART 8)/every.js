a3e1a662e1a6ae8941e9332b2851c59dbf2e6399
KISHLAY568 <kishlayk568@gmail.com>
2024-02-08 16:45:11 +0530

    first commit

---
+++ JS(PART 8)/every.js
@@ -0,0 +1,12 @@
+let arr = [2, 4, 6, 8];
+let result = arr.every((el) => {
+    return el % 2 == 0;
+})
+console.log(result);
+
+
+let arr2 = [2, 1, 6, 8];
+let result2 = arr2.every((el) => {
+    return el % 2 == 0;
+})
+console.log(result2);
