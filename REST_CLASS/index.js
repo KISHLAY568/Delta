81b9267a2d5a0e81d65d1560362458fb0bffb208
KISHLAY568 <kishlayk568@gmail.com>
2024-03-20 19:44:14 +0530

    first commit

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -32,7 +32,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
   let { username, content } = req.body;
   posts.push({ username, content });
-  res.send("post request is working");
+  res.redirect("/posts");
 });
 app.listen(port, () => {
   console.log("listening to port 3000");
