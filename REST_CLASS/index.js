0a933e75d2a6baf32a1310b3027ce5cfa1a9d160
KISHLAY568 <kishlayk568@gmail.com>
2024-03-20 17:42:51 +0530

    first commit

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -8,7 +8,24 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set(express.static(path.join(__dirname, "public")));
+let posts = [
+  {
+    username: "kishlay",
+    content: "I love coding",
+  },
+  {
+    username: "brocklesnar",
+    content: "eat sleep conquer repeat",
+  },
+  {
+    username: "jhoncena",
+    content: "you can't see me",
+  },
+];
 
+app.get("/posts", (req, res) => {
+  res.render("index.ejs");
+});
 app.listen(port, () => {
   console.log("listening to port 3000");
 });
