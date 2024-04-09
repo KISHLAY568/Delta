907f77f79e785daf223f67536d0d8ed3f4fabffa
KISHLAY568 <kishlayk568@gmail.com>
2024-04-09 20:21:22 +0530

    first commit

---
+++ mongo0/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/test");
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const user2 = new User({
+  name: "Eve",
+  email: "eve@yahoo.in",
+  age: 48,
+});
+
+user2
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
