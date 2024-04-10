ca5ae1b2a3567940b4036fce63c55902c9525616
KISHLAY568 <kishlayk568@gmail.com>
2024-04-10 19:51:52 +0530

    first commit

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -45,6 +45,11 @@ app.get("/chats", async (req, res) => {
   res.render("index.ejs", { chats });
 });
 
+//New Route
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 app.listen(3000, () => {
   console.log("server is listening to port 3000");
 });
