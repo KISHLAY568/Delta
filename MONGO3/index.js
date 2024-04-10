973863d6d381340f43196e94a272d6768102f1fd
KISHLAY568 <kishlayk568@gmail.com>
2024-04-10 13:43:00 +0530

    first commit

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -15,6 +16,22 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+let chat1 = new Chat({
+  from: "neha",
+  to: "priya",
+  msg: "send me your exam sheets",
+  created_at: new Date(),
+});
+
+chat1
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("working");
