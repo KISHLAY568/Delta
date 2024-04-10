41b1c82e31e447f08e22678e8d51beb2153980ab
KISHLAY568 <kishlayk568@gmail.com>
2024-04-10 14:33:05 +0530

    first commit

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
   .then(() => {
@@ -17,24 +18,31 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
-let chat1 = new Chat({
-  from: "neha",
-  to: "priya",
-  msg: "send me your exam sheets",
-  created_at: new Date(),
-});
+// let chat1 = new Chat({
+//   from: "neha",
+//   to: "priya",
+//   msg: "send me your exam sheets",
+//   created_at: new Date(),
+// });
 
-chat1
-  .save()
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// chat1
+//   .save()
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 app.get("/", (req, res) => {
   res.send("working");
+});
+
+//show all chats Index Route
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", { chats });
 });
 
 app.listen(3000, () => {
