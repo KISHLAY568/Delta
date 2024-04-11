8dc0af9b9001273f3ffd613452dd322046303060
KISHLAY568 <kishlayk568@gmail.com>
2024-04-11 10:03:36 +0530

    first commit

---
+++ MAJORPROJECT/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.listen(3000, () => {
+  console.log("server is listening to port 3000");
+});
