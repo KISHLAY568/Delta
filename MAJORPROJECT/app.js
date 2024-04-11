f9fd4c4181bb59d84d386479569aae3b641da462
KISHLAY568 <kishlayk568@gmail.com>
2024-04-11 14:30:16 +0530

    first commit

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -81,6 +81,14 @@ app.put("/listings/:id", async (req, res) => {
   res.redirect(`/listings/${id}`);
 });
 
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
 app.listen(3000, () => {
   console.log("server is listening to port 3000");
 });
