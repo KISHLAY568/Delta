d0a76ccdaa5753b95a88cb112e12332031e6f86c
KISHLAY568 <kishlayk568@gmail.com>
2024-04-12 17:22:19 +0530

    first commit

--- MIDDLEWARES/app.js
+++ MIDDLEWARES/app.js
@@ -21,15 +21,23 @@ const app = express();
 //   next();
 // });
 
-app.use("/api", (req, res, next) => {
+// app.use("/api", (req, res, next) => {
+//   let { token } = req.query;
+//   if (token === "giveaccess") {
+//     next();
+//   }
+//   res.send("ACCESS DENIED!");
+// });
+
+const checkToken = (req, res, next) => {
   let { token } = req.query;
   if (token === "giveaccess") {
     next();
   }
   res.send("ACCESS DENIED!");
-});
+};
 
-app.get("/api", (req, res) => {
+app.get("/api", checkToken, (req, res) => {
   res.send("data");
 });
 
