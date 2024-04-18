000b39a7fed941d2106b9e1f3df05a76ecce39e6
KISHLAY568 <kishlayk568@gmail.com>
2024-04-18 20:54:56 +0530

    first commit

--- MAJORPROJECT/routes/listing.js
+++ MAJORPROJECT/routes/listing.js
@@ -69,6 +69,7 @@ router.put(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
   })
 );
@@ -80,6 +81,7 @@ router.delete(
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   })
 );
