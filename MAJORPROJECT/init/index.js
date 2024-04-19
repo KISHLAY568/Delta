85b1f658eb24889a05afd6cf2b6ab62d836b0bbb
KISHLAY568 <kishlayk568@gmail.com>
2024-04-19 16:22:49 +0530

    first commit

--- MAJORPROJECT/init/index.js
+++ MAJORPROJECT/init/index.js
@@ -16,6 +16,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "662248622bf17e2e831f6035",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
