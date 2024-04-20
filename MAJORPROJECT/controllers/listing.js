ab00ffe8ea0320393088a1b58441cae61b09dfba
KISHLAY568 <kishlayk568@gmail.com>
2024-04-20 22:33:50 +0530

    first commit

--- MAJORPROJECT/controllers/listing.js
+++ MAJORPROJECT/controllers/listing.js
@@ -32,14 +32,12 @@ module.exports.createListing = async (req, res, next) => {
     })
     .send();
 
-  console.log(response.body.features[0].geometry);
-  res.send("done!");
-
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
+  newListing.geometry = response.body.features[0].geometry;
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
