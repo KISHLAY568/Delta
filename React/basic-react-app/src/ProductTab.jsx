8469277f787e14c6bae0dea79dc3c82437cc2b7d
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 22:17:51 +0530

    first commit

--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -4,8 +4,8 @@ function ProductTab() {
   return (
     <>
       <Product title="phone" price={30000} features={options} />
-      <Product title="laptop" price={50000} />
-      <Product title="pen" price={1} />
+      {/* <Product title="laptop" price={50000} />
+      <Product title="pen" price={1} /> */}
     </>
   );
 }
