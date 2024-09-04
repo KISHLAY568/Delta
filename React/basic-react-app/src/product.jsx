c80f4ba6f5f53639eef08f80ae1ee79f9ff83278
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 23:32:43 +0530

    first commit

--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -5,11 +5,12 @@ function Product({ title, price, features }) {
     <div className="Product">
       <h3>{title}</h3>
       <h5>price : {price}</h5>
-      <p>
+      {price > 30000 && <p>Discount of 5%</p>}
+      {/* <p>
         {features.map((feature) => (
           <li>{feature}</li>
         ))}
-      </p>
+      </p> */}
       {/* <p>{list}</p> */}
     </div>
   );
