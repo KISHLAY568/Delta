898ab05d31b111f6e1099e923ead284c1f603f4e
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 23:41:57 +0530

    first commit

--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -1,11 +1,13 @@
 import "./Product.css";
 function Product({ title, price, features }) {
   // const list = features.map((feature) => <li>{feature}</li>);
+  let isDiscount = price > 3000;
+  let styles = { backgroundColor: isDiscount ? "pink" : "" };
   return (
-    <div className="Product">
+    <div className="Product" style={styles}>
       <h3>{title}</h3>
       <h5>price : {price}</h5>
-      {price > 30000 && <p>Discount of 5%</p>}
+      {isDiscount && <p>Discount of 5%</p>}
       {/* <p>
         {features.map((feature) => (
           <li>{feature}</li>
