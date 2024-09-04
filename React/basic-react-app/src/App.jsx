015792da22f81d243959bb3e99447977d5e1761b
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 23:51:27 +0530

    first commit

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import Title from "./Title";
 import ProductTab from "./ProductTab";
+import MsgBox from "./MsgBox";
 function App() {
-  return <ProductTab />;
+  return (
+    <>
+      <MsgBox username="Kishlay" textcolor="yellow" />
+      <ProductTab />
+    </>
+  );
 }
 
 export default App;
