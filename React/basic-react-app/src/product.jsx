f4fd458d776b6bc3eb2e854833bc3fa19def25c8
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 21:44:47 +0530

    first commit

--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -1,9 +1,9 @@
 import "./Product.css";
-function Product(props) {
-  console.log(props)
+function Product({title}) {
+  
   return (
     <div className="Product">
-      <h3>Product Title</h3>
+      <h3>{title}</h3>
       <h5>Product Description</h5>
     </div>
   );
