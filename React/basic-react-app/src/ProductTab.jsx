b361e96771d76471f4f5a1183696914315c58486
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 21:55:35 +0530

    first commit

--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -1,10 +1,11 @@
 import Product from "./product";
 function ProductTab() {
+  let options = ["hi-tech", "durable", "fast"];
   return (
     <>
-      <Product title="phone" price={(30000)} />
-      <Product title="laptop" price={(50000)} />
-      <Product title="pen" />
+      <Product title="phone" price={30000} features={options} />
+      <Product title="laptop" price={50000} />
+      <Product title="pen" price={1} />
     </>
   );
 }
