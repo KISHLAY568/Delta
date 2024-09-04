61d090d55b5ec272c18a86750d839032c54cd9ad
KISHLAY568 <kishlayk568@gmail.com>
2024-09-04 22:07:30 +0530

    first commit

--- React/basic-react-app/src/product.jsx
+++ React/basic-react-app/src/product.jsx
@@ -1,9 +1,11 @@
 import "./Product.css";
-function Product({ title, price = 1 }) {
+function Product({ title, price, features }) {
+  const list = features.map((feature) => <li>{feature}</li>);
   return (
     <div className="Product">
       <h3>{title}</h3>
       <h5>price : {price}</h5>
+      <p>{features}</p>
     </div>
   );
 }
