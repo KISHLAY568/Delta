b193618db93ad3c4ebdcc290f42f8c5adb60c9fa
KISHLAY568 <kishlayk568@gmail.com>
2024-09-06 00:05:58 +0530

    first commit

--- React/basic-react-app/src/Product2.jsx
+++ React/basic-react-app/src/Product2.jsx
@@ -1,8 +1,13 @@
 import "./Product.css";
-function Product2() {
+import Price from "./Price";
+function Product2({ title, idx }) {
+  let oldPrices = ["12,495", "11,900", "1,599", "1,999"];
+  let newPrices = ["9,999", "9,999", "1,299", "1,499"];
   return (
     <div className="Product">
-      <p>product</p>
+      <h1>{title}</h1>
+      <p>desccription</p>
+      <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]} />
     </div>
   );
 }
--- React/basic-react-app/src/ProductList.jsx
+++ React/basic-react-app/src/ProductList.jsx
@@ -2,10 +2,10 @@ import Product2 from "./Product2";
 function ProductTab() {
   return (
     <>
-      <Product2 />
-      <Product2 />
-      <Product2 />
-      <Product2 />
+      <Product2 title="Logitech MX Master" idx={0} />
+      <Product2 title="Apple Pencil (2nd Gen)" idx={1} />
+      <Product2 title="Zebronics Zeb-transformer" idx={2} />
+      <Product2 title="Petronics Toad 23" idx={3} />
     </>
   );
 }
