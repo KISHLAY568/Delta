849815b57033ab9babff84a7147af25f88892aad
KISHLAY568 <kishlayk568@gmail.com>
2024-09-06 00:25:19 +0530

    first commit

--- React/basic-react-app/src/ProductList.jsx
+++ React/basic-react-app/src/ProductList.jsx
@@ -7,12 +7,12 @@ function ProductTab() {
     alignItems: "center",
   };
   return (
-    <>
+    <div style={styles}>
       <Product2 title="Logitech MX Master" idx={0} />
       <Product2 title="Apple Pencil (2nd Gen)" idx={1} />
       <Product2 title="Zebronics Zeb-transformer" idx={2} />
       <Product2 title="Petronics Toad 23" idx={3} />
-    </>
+    </div>
   );
 }
 
