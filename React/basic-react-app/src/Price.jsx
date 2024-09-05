6146256617ce870fc86611860a7b4ade79cbff76
KISHLAY568 <kishlayk568@gmail.com>
2024-09-06 00:12:53 +0530

    first commit

--- React/basic-react-app/src/Price.jsx
+++ React/basic-react-app/src/Price.jsx
@@ -2,7 +2,7 @@ export default function Price({ oldPrice, newPrice }) {
   return (
     <div>
       <span>{oldPrice}</span>
-      &nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;
       <span>{newPrice} </span>
     </div>
   );
--- React/basic-react-app/src/Product2.jsx
+++ React/basic-react-app/src/Product2.jsx
@@ -3,10 +3,16 @@ import Price from "./Price";
 function Product2({ title, idx }) {
   let oldPrices = ["12,495", "11,900", "1,599", "1,999"];
   let newPrices = ["9,999", "9,999", "1,299", "1,499"];
+  let desccription = [
+    "8,000 DPI",
+    "intiutive surface",
+    "designed for ipad pro",
+    "wireless",
+  ];
   return (
     <div className="Product">
       <h1>{title}</h1>
-      <p>desccription</p>
+      <p>{desccription[idx]}</p>
       <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]} />
     </div>
   );
