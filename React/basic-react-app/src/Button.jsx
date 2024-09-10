bc36aea0eaf7d138e12d5cfbc8963bf1cd9179a8
KISHLAY568 <kishlayk568@gmail.com>
2024-09-10 18:40:32 +0530

    react

--- React/basic-react-app/src/Button.jsx
+++ React/basic-react-app/src/Button.jsx
@@ -2,10 +2,23 @@ function printHello() {
   console.log("HELLO!");
 }
 
+function handleMouseOver() {
+  console.log("Bye!");
+}
+function handleDblClick() {
+  console.log("Dbl click");
+}
+
 export default function Button() {
   return (
     <div>
       <button onClick={printHello}>click me!</button>
+      <p onMouseOver={handleMouseOver}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ullam sed
+        distinctio maiores, nulla, rem deleniti doloribus porro nostrum tenetur
+        ducimus dolores rerum quidem architecto, harum quo sit autem blanditiis.
+      </p>
+      <button onDoubleClick={handleDblClick}>Double click Me</button>
     </div>
   );
 }
