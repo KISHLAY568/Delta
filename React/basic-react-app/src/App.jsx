f1a1271d857e693b9195f7493216c8c8dd721d2d
KISHLAY568 <kishlayk568@gmail.com>
2024-09-10 18:26:42 +0530

    react

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
+import Button from "./Button";
 // import Title from "./Title";
 // import ProductTab from "./ProductTab";
-import ProductList from "./ProductList";
+// import ProductList from "./ProductList";
 // import MsgBox from "./MsgBox";
 function App() {
   return (
     <>
-      {/* <MsgBox username="Kishlay" textcolor="yellow" />
-      <MsgBox username="Kishan" textcolor="lime" /> */}
-      <h2>Blockbuster Deals | Shop Now</h2>
-      <ProductList />
+      <Button />
     </>
   );
 }
